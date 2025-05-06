bb9241ed5c98ae02ed6d8d22056297d62128b6ba
hamdi4-beep <hamdiakippuden@gmail.com>
2025-05-06 11:39:02 +0100

    added the assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import data from './data.json'
+
 function App() {
+  console.log(data)
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <img src={data.currentUser.image.png} alt="" />
+    </div>
   )
 }
 
